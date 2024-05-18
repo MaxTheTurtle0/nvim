e64322ed3064d11b0a7f8e1599eeaee7d20b4cfb
MaxTheTurtle0 <m.andrei.antonescu@gmail.com>
2024-05-18 10:48:40 +0200

    add remaps

--- lua/maxtheturtle0/remap.lua
+++ lua/maxtheturtle0/remap.lua
@@ -4,6 +4,12 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
 
 vim.api.nvim_set_keymap('i', '(', '()<Left>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '{', '{}<Left>', { noremap = true, silent = true })
